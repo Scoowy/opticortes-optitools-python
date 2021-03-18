03b572e7dfdea4859fa4e075329ae4c9a60449a4
Scoowy <gahonajuanjo@gmail.com>
2021-03-17 22:52:31 -0500

    Implement a HalfPlank View Tab functionalities.
    Changes:
    - Added HalfPlank controller.
    - Added HalfPlank model.
    - Implemented a HalfPlank logic.
    
    Bugs:
    - Number of decimals from 2 to 4, in money values.

---
+++ controller/tabs/C_HalfPlankController.py
@@ -0,0 +1,16 @@
+from PyQt5.QtCore import QObject, pyqtSlot
+
+from model.tabs.M_HalfPlankController import HalfPlankModel
+
+
+class HalfPlankController(QObject):
+    def __init__(self, model: HalfPlankModel) -> None:
+        super().__init__()
+
+        self._model = model
+
+    def calculateCostReal(self, cost: float, discount: float):
+        self._model.calculateCostReal(cost, discount)
+
+    def calculatePvp(self, costReal: float):
+        self._model.calculatePVP(costReal)
